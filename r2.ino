ba1f8b2b36afa477c38286b5a301e0df0ec63db7
Richard Narbonne <richard.narbonne@gmail.com>
2015-05-04 21:29:27 +0200

    2015 04 15

--- r2.ino
+++ r2.ino
@@ -1,4 +1,3 @@
-#include <SimpleTimer.h>
 #include <Arduino.h>
 
 const int AVANT = HIGH;
@@ -12,221 +11,218 @@ const int brocheFreinMoteurR = 8;
 
 const int brocheThro = 4;
 const int brocheAile = 5;
-const int brocheINT4 = 19;
-const int brocheINT5 = 18;
+const int brocheINT4 = 18;
+const int brocheINT5 = 19;
 
 const int kTics = 150;
 
-int encoderR = 0;
-int encoderL = 0;
+volatile int encoderR = 0;
+volatile int encoderL = 0;
 
-SimpleTimer timer;
+void setup()  {
+  Serial.begin(9600);
+  pinMode(brocheINT4, INPUT);
+  pinMode(brocheINT5, INPUT);
+  pinMode(brocheThro, INPUT);
+  pinMode(brocheAile, INPUT);
 
-void setup() {
-	Serial.begin(9600);
-	pinMode(brocheINT4, INPUT);
-	pinMode(brocheINT5, INPUT);
-	pinMode(brocheThro, INPUT);
-	pinMode(brocheAile, INPUT);
+  pinMode(brocheVitesseMoteurL, OUTPUT);
+  pinMode(brocheDirMoteurL, OUTPUT);
+  pinMode(brocheFreinMoteurL, OUTPUT);
+  pinMode(brocheVitesseMoteurR, OUTPUT);
+  pinMode(brocheDirMoteurR, OUTPUT);
+  pinMode(brocheFreinMoteurR, OUTPUT);
 
-	pinMode(brocheVitesseMoteurL, OUTPUT);
-	pinMode(brocheDirMoteurL, OUTPUT);
-	pinMode(brocheFreinMoteurL, OUTPUT);
-	pinMode(brocheVitesseMoteurR, OUTPUT);
-	pinMode(brocheDirMoteurR, OUTPUT);
-	pinMode(brocheFreinMoteurR, OUTPUT);
+  digitalWrite(brocheDirMoteurL, AVANT);
+  digitalWrite(brocheDirMoteurR, AVANT);
+  digitalWrite(brocheFreinMoteurL, LOW);
+  digitalWrite(brocheFreinMoteurR, LOW);
 
-	digitalWrite(brocheDirMoteurL, AVANT);
-	digitalWrite(brocheDirMoteurR, AVANT);
-	digitalWrite(brocheFreinMoteurL, LOW);
-	digitalWrite(brocheFreinMoteurR, LOW);
-
-	attachInterrupt(4, intEncoderL, CHANGE);
-	attachInterrupt(5, intEncoderR, CHANGE);
-
-	timer.setInterval(100, repeat2);
+  attachInterrupt(4, intEncoderL, CHANGE); //PIN 19
+  attachInterrupt(5, intEncoderR, CHANGE); //PIN 18
 }
 
-void repeat2() {
+void repeat(void) {
+  static float kP = 1;
+  static float kI = 0;
+  static float kD = 0;
+  static unsigned long timeMillis;
+  static unsigned long previousTimeMillis;
 
-	static float kP = 0;
-	static float kI = 0;
-	static float kD = 0;
+  static int iScenar = 0;
+  static int throVal = 0;
+  static int aileVal = 0;
+  static int previousXR = 0;
+  static int previousXL = 0;
+  static int dxR = 0;
+  static int dxL = 0;
+  static int puissanceMoteurs = 0;
+  static int puissanceVirtuelleMoteurL = 0;
+  static int puissanceVirtuelleMoteurR = 0;
+  static int puissanceReelleMoteurL = 0;
+  static int puissanceReelleCompenseeMoteurL = 0;
+  static int sensMoteurL = AVANT;
+  static int puissanceReelleMoteurR = 0;
+  static int puissanceReelleCompenseeMoteurR = 0;
+  static int sensMoteurR = AVANT;
+  static int direction = 0;
+  static int vL;
+  static int vR;
+  static int errL;
+  static int errR;
+  static int deltaErrL = 0;
+  static int deltaErrR = 0;
+  static int errSumL = 0;
+  static int errSumR = 0;
 
-	static int iScenar = 0;
-	static int throVal = 0;
-	static int aileVal = 0;
-	static int previousXR = 0;
-	static int previousXL = 0;
-	static int dxR = 0;
-	static int dxL = 0;
-	static unsigned long timeMillis;
-	static unsigned long previousTimeMillis;
-	static int puissanceMoteurs = 0;
-	static int puissanceVirtuelleMoteurL = 0;
-	static int puissanceVirtuelleMoteurR = 0;
-	static int puissanceReelleMoteurL = 0;
-	static int puissanceReelleCompenseeMoteurL = 0;
-	static int sensMoteurL = AVANT;
-	static int puissanceReelleMoteurR = 0;
-	static int puissanceReelleCompenseeMoteurR = 0;
-	static int sensMoteurR = AVANT;
-	static int direction = 0;
-	static int vL;
-	static int vR;
-	static int errL;
-	static int errR;
-	static int deltaErrL = 0;
-	static int deltaErrR = 0;
-	static int errSumL = 0;
-	static int errSumR = 0;
+  timeMillis = millis();
+  int dt = timeMillis - previousTimeMillis;
 
-	int scenar1;
+  dxL = encoderL - previousXL;
+  vL = (unsigned long) kTics * dxL / dt;
 
-	timeMillis = millis();
-	int dt = timeMillis - previousTimeMillis;
+  dxR = encoderR - previousXR;
+  vR = (unsigned long) kTics * dxR / dt;
 
-	dxL = encoderL - previousXL;
-	vL = (unsigned long) kTics * dxL / dt;
+  //throVal = getAverage(pulseIn(brocheThro, HIGH, 25000));
+  throVal = pulseIn(brocheThro, HIGH, 25000);
+  aileVal = pulseIn(brocheAile, HIGH, 25000);
 
-	dxR = encoderR - previousXR;
-	vR = (unsigned long) kTics * dxR / dt;
+  puissanceMoteurs = map(throVal, 1087, 1880, -10, 10);
+  direction = map(aileVal, 1110, 1865, -2, 2);
+  //traceVal(throVal);
+  //traceVal(direction);
 
+  puissanceMoteurs = 4;
+  traceVal(puissanceMoteurs);
 
-	throVal = getAverage(pulseIn(brocheThro, HIGH, 25000));
-	aileVal = pulseIn(brocheAile, HIGH, 25000);
+  direction = 0;
 
-	puissanceMoteurs = map(throVal, 1087, 1880, -255, 255);
+  puissanceVirtuelleMoteurL = puissanceMoteurs - direction;
+  puissanceVirtuelleMoteurR = puissanceMoteurs + direction;
 
-	scenar1 = (iScenar%20)-10;
-	scenar1 = scenar1==0?1:scenar1;
-	puissanceMoteurs = 20 * scenar1/abs(scenar1);
-	kP = (iScenar/20) - (iScenar/20)%20;
+  sensMoteurL = computeSensMoteur(puissanceVirtuelleMoteurL);
+  puissanceReelleMoteurL = abs(puissanceVirtuelleMoteurL);
+  sensMoteurR = computeSensMoteur(puissanceVirtuelleMoteurR);
+  puissanceReelleMoteurR = abs(puissanceVirtuelleMoteurR);
 
-	direction = map(aileVal, 1086, 1865, -1000, 1000);
-	direction = 0;
+  deltaErrL = errL;
+  errL = puissanceReelleMoteurL - vL;
+  deltaErrL -= errL;
+  errSumL += errL;
+  traceVal(vL);
+    traceVal(errL);
+  puissanceReelleCompenseeMoteurL += kP * errL ;// + kI * errSumL + kD * deltaErrL;
+  puissanceReelleCompenseeMoteurL = max(0, min(255, puissanceReelleCompenseeMoteurL));
 
-	puissanceVirtuelleMoteurL = puissanceMoteurs - direction;
-	puissanceVirtuelleMoteurR = puissanceMoteurs + direction;
-	puissanceVirtuelleMoteurR = 0;
+  deltaErrR = errR;
+  errR = puissanceReelleMoteurR - vR;
+  deltaErrR -= errR;
+  errSumR += errR;
+  puissanceReelleCompenseeMoteurR += kP * errR ;// + kI * errSumR + kD * deltaErrR;
+  puissanceReelleCompenseeMoteurR = max(0, min(255, puissanceReelleCompenseeMoteurR));
+/*
+  if (iScenar > 20 || iScenar > 50) {
+    puissanceReelleCompenseeMoteurL = 0;
+    puissanceReelleCompenseeMoteurR = 0;
+  }
+*/
+  digitalWrite(brocheDirMoteurL, sensMoteurL);
+  digitalWrite(brocheDirMoteurR, sensMoteurR);
+  traceVal(puissanceReelleCompenseeMoteurL);
+  analogWrite(brocheVitesseMoteurL, puissanceReelleCompenseeMoteurL);
+  analogWrite(brocheVitesseMoteurR, puissanceReelleCompenseeMoteurR);
 
-	sensMoteurL = computeSensMoteur(puissanceVirtuelleMoteurL);
-	puissanceReelleMoteurL = abs(puissanceVirtuelleMoteurL);
-	sensMoteurR = computeSensMoteur(puissanceVirtuelleMoteurR);
-	puissanceReelleMoteurR = abs(puissanceVirtuelleMoteurR);
+  previousXL = encoderL;
+  previousXR = encoderR;
+  previousTimeMillis = timeMillis;
+  /*
+    trace(iScenar,timeMillis,kP,kI,kD,puissanceVirtuelleMoteurL,puissanceVirtuelleMoteurR,puissanceReelleMoteurL,puissanceReelleMoteurR,puissanceReelleCompenseeMoteurL,sensMoteurL,
+  		  puissanceReelleCompenseeMoteurR,sensMoteurR,vL,vR,errL,errR,errSumL,errSumR,deltaErrL,deltaErrR,
+  		  encoderL,encoderR);
+  */
+  Serial.println("");
+  iScenar++;
+}
 
-	//if(puissanceReelleMoteurL<20) puissanceReelleMoteurL=0;
-	//if(puissanceReelleMoteurB<20) puissanceReelleMoteurB=0;
+void trace(int v1, unsigned long t, float v2, float v3, float v4, int v5, int v6, int v7, int v8, int v9, int v10, int v11, int v12, int v13, int v14, int v15, int v16, int v17, int v18, int v19, int v20, int
+           v21, int v22) {
+  traceVal(v1);
+  traceVal(t);
+  traceVal(v2);
+  traceVal(v3);
+  traceVal(v4);
+  traceVal(v5);
+  traceVal(v6);
+  traceVal(v7);
+  traceVal(v8);
+  traceVal(v9);
+  traceVal(v10);
+  traceVal(v11);
+  traceVal(v12);
+  traceVal(v13);
+  traceVal(v14);
+  traceVal(v15);
+  traceVal(v16);
+  traceVal(v17);
+  traceVal(v18);
+  traceVal(v19);
+  traceVal(v20);
+  traceVal(v21);
+  traceVal(v22);
+  Serial.println("");
+}
 
-	//Serial.print(puissanceReelleMoteurL);
-	//Serial.print(':');
-	deltaErrL = errL;
-	errL = puissanceReelleMoteurL - vL;
-	deltaErrL -= errL;
-	errSumL += errL;
-	puissanceReelleMoteurL += kP * errL + kI * errSumL + kD * deltaErrL;
-	puissanceReelleMoteurL = max(0, min(255, puissanceReelleMoteurL));
+void traceVal(float val) {
+  Serial.print(val);
+  Serial.print(':');
+}
 
-	deltaErrR = errR;
-	errR = puissanceReelleMoteurR - vR;
-	deltaErrR -= errR;
-	errSumR += errR;
-	puissanceReelleCompenseeMoteurR += kP * errR + kI * errSumR + kD * deltaErrR;
-	puissanceReelleCompenseeMoteurR = max(0, min(255, puissanceReelleMoteurR));
+void traceVal(int val) {
+  Serial.print(val);
+  Serial.print(':');
+}
 
-	//Serial.print(errL + 100);
-	//Serial.print(':');
-
-	//Serial.print(errSumL + 150);
-//  Serial.print(':');
-
-//  Serial.print(puissanceReelleMoteurL);
-//  Serial.print(':');
-
-	digitalWrite(brocheDirMoteurL, sensMoteurL);
-	digitalWrite(brocheDirMoteurR, sensMoteurR);
-	analogWrite(brocheVitesseMoteurL, puissanceReelleCompenseeMoteurL);
-	analogWrite(brocheVitesseMoteurR, puissanceReelleCompenseeMoteurR);
-
-	previousXL = encoderL;
-	previousXR = encoderR;
-	previousTimeMillis = timeMillis;
-
-	Serial.print(iScenar);
-	Serial.print(':');
-	Serial.print(kP);
-	Serial.print(':');
-	Serial.print(kI);
-	Serial.print(':');
-	Serial.print(kD);
-	Serial.print(':');
-	Serial.print(puissanceVirtuelleMoteurL);
-	Serial.print(':');
-	Serial.print(puissanceVirtuelleMoteurR);
-	Serial.print(':');
-	Serial.print(puissanceReelleMoteurL);
-	Serial.print(':');
-	Serial.print(puissanceReelleMoteurR);
-	Serial.print(':');
-	Serial.print(puissanceReelleCompenseeMoteurL);
-	Serial.print(':');
-	Serial.print(sensMoteurL);
-	Serial.print(':');
-	Serial.print(puissanceReelleCompenseeMoteurR);
-	Serial.print(':');
-	Serial.print(sensMoteurR);
-	Serial.print(':');
-	Serial.print(vL);
-	Serial.print(':');
-	Serial.print(vR);
-	Serial.print(':');
-	Serial.print(errL);
-	Serial.print(':');
-	Serial.print(errR);
-	Serial.print(':');
-	Serial.print(errSumL);
-	Serial.print(':');
-	Serial.print(errSumR);
-	Serial.print(':');
-	Serial.print(deltaErrL);
-	Serial.print(':');
-	Serial.print(deltaErrR);
-
-	Serial.println("");
-
-	iScenar++;
+void traceVal(unsigned long val) {
+  Serial.print(val);
+  Serial.print(':');
 }
 
 int getAverage(int val) {
-	static int vals[10] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-	static int index = 0;
-	static int average;
-	vals[index] = val;
+  static int vals[10] = {
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+  };
+  static int index = 0;
+  static int average;
+  vals[index] = val;
 
-	index = (index + 1) % 10;
-	average = 0;
-	for (int i = 0; i < 10; i++) {
-		average += vals[i];
-	}
-	return average / 10;
+  index = (index + 1) % 10;
+  average = 0;
+  for (int i = 0; i < 10; i++) {
+    average += vals[i];
+  }
+  return average / 10;
 }
 
 void loop() {
-	timer.run();
+  repeat();
 }
 
 void intEncoderL() {
-	encoderL++;
+  encoderL++;
 }
 
 void intEncoderR() {
-	encoderR++;
+  encoderR++;
 }
 
 int computeSensMoteur(int p) {
-	if (p > 0) {
-		return AVANT;
-	}
-	return ARRIERE;
+  if (p > 0) {
+    return AVANT;
+  }
+  return ARRIERE;
 }
+
+
+
 
